a9a9627ce828346d30191f047d1c03008a77ae33
francoposa <franco@francoposa.io>
2024-03-01 21:09:44 -0800

    copy in basic tailwind config from francoposa.io site; convert homepage to basic form submit; add tailwind flexbox styling to center form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: [
@@ -7,11 +8,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['Work Sans', ...defaultTheme.fontFamily.sans],
+        mono: ['Courier Prime', ...defaultTheme.fontFamily.mono],
       },
     },
   },
