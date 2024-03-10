5fec766ad7471839cf9e0d4d66ff12fa9476c1fd
francoposa <franco@francoposa.io>
2024-03-09 17:48:45 -0800

    actually post the value and show response! TODO error handling probably

--- app/page.tsx
+++ app/page.tsx
@@ -3,39 +3,47 @@
 import React, { useState } from 'react';
 import FixedTextArea from "@/components/FixedTextArea";
 
-interface FormData {
-  textField: string;
-}
+const apiURL: string = process.env.NEXT_PUBLIC_ECHO_SERVER_API_BASE_URL || "http://localhost";
+const submitTypeRaw = 'raw';
+const submitTypeJSON = 'json';
 
 const App: React.FC = () => {
   const [textInputValue, setTextInputValue] = useState('');
+  const [textOutputValue, setTextOutputValue] = useState('');
 
   const handleTextInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTextInputValue(e.target.value);
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>, submitType: string) => {
     e.preventDefault();
 
     try {
-      const responseData = await submitFormToApi({ textField: textInputValue });
-      // Handle success
-      console.log('Form submitted successfully:', responseData);
+      let url = apiURL;
+      if (submitType === submitTypeJSON) {
+        url += '/json';
+      }
+      console.log(url);
+
+      const responseData = await submitFormToApi({ textInputValue: textInputValue }, url);
+      if (submitType === submitTypeJSON) {
+        setTextOutputValue(JSON.stringify(responseData));
+      } else {
+        setTextOutputValue(responseData);
+      }
+
     } catch (error) {
-      // Handle error
       console.error('Error submitting form:', error);
     }
   };
 
-  const submitFormToApi = async (requestData: FormData): Promise<any> => {
-    const apiUrl = 'https://example.com/api/submit';
-
+  const submitFormToApi = async (requestData: { textInputValue: string; }, apiUrl: string): Promise<any> => {
     const response = await fetch(apiUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(requestData)
+      body: JSON.stringify(requestData.textInputValue)
     });
 
     if (!response.ok) {
@@ -47,21 +55,22 @@ const App: React.FC = () => {
 
   return (
       <div className="container max-w-6xl border border-stone-200 bg-white font-mono">
-          <div className="flex flex-row flex-wrap">
-            <div className="basis-full text-center">
-              <h1 className="mt-8 mb-4 text-4xl font-mono">backtalk.dev</h1>
-            </div>
+        <div className="flex flex-row flex-wrap">
+          <div className="basis-full text-center">
+            <h1 className="mt-8 mb-4 text-4xl font-mono">backtalk.dev</h1>
           </div>
+        </div>
         <hr className="mx-8"></hr>
         <div className="py-8">
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={(e) => handleSubmit(e, submitTypeRaw)}>
             <div className="flex flex-row flex-wrap justify-center">
               <div className="basis-1/2 text-center">
                 <div className="lg:basis-1/2 basis-full">
-                  <label htmlFor="textField">Enter Text:</label>
+                  <label htmlFor="textInput">Enter Text:</label>
                 </div>
                 <div className="lg:basis-1/2 basis-full">
                   <FixedTextArea
+                      id="textInput"
                       rows={16}
                       cols={40}
                       value={textInputValue}
@@ -71,29 +80,33 @@ const App: React.FC = () => {
               </div>
               <div className="basis-1/2 text-center">
                 <div className="lg:basis-1/2 basis-full">
-                  <label htmlFor="textField">Output:</label>
+                  <label htmlFor="textOutput">Output:</label>
                 </div>
                 <div className="lg:basis-1/2 basis-full">
-                  <textarea
-                      // type="text"
+                  <FixedTextArea
                       id="textOutput"
-                    name="textOutput"
-                    // value={textOutputValue}
-                    // onChange={handleInputChange}
-                    // required
-                      className="w-3/4 h-72 rounded border border-stone-200"
-                />
+                      rows={16}
+                      cols={40}
+                      value={textOutputValue}
+                      onChange={null}
+                      readOnly={true}
+                  />
                 </div>
               </div>
             </div>
             <div className="flex flex-row flex-wrap justify-center my-8">
-              <button type="submit" className="lg:basis-1/6 basis-1/2 rounded border border-stone-200">
+              <button type="submit" id="submitRaw" className="lg:basis-1/6 basis-1/2 rounded border border-stone-200">
                 Submit Raw
               </button>
               <div className="lg:basis-1/12 basis-full my-2"></div>
-              <button type="submit" className="lg:basis-1/6 basis-1/2 rounded border border-stone-200">
+            </div>
+          </form>
+          <form onSubmit={(e) => handleSubmit(e, submitTypeJSON)}>
+            <div className="flex flex-row flex-wrap justify-center my-8">
+              <button type="submit" id="submitJSON" className="lg:basis-1/6 basis-1/2 rounded border border-stone-200">
                 Submit JSON
               </button>
+              <div className="lg:basis-1/12 basis-full my-2"></div>
             </div>
           </form>
         </div>
--- components/FixedTextArea.js
+++ components/FixedTextArea.js
@@ -1,11 +1,13 @@
-const FixedTextArea = ({ rows, cols, value, onChange }) => {
+const FixedTextArea = ({ id, rows, cols, value, onChange, readOnly = false }) => {
     return (
         <textarea
+            id={id}
             rows={rows}
             cols={cols}
             className="resize-none w-3/4 h-72 rounded border border-stone-200"
             value={value}
             onChange={onChange}
+            readOnly={readOnly}
         />
     );
 };
